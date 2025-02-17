d66b0c9ec96f180cdf6b17304bb9b076426c2c34
Dark-ue <pandeygsphy1@gmail.com>
2025-02-17 10:40:17 +0530

    ping optimisation

--- command/ping.py
+++ command/ping.py
@@ -1,15 +1,10 @@
 import discord
 from discord.ext import commands
-import time
 
 def ping(bot):
     @bot.command()
     async def ping(ctx):
-        start_time = time.time()
-        message = await ctx.send('Pinging...')
-        end_time = time.time()
-        response_time = (end_time - start_time) * 1000  # Convert to milliseconds
-        await message.edit(content=f'Pong! Response time: `{response_time:.2f} ms`')
+        await ctx.send(f'Pong! Latency: `{int(bot.latency * 1000)} ms`')
 
 #list of commands
 #ping
--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import os
-os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
-
-#import modules
+import logging
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from command import ping
 from command.moderation import warn, mod_event
 
-#load the token from the .env file
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Prevent Python from writing .pyc files
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+# Load the token from the .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 if TOKEN is None:
     raise ValueError("No token found. Please set the DISCORD_TOKEN environment variable.")
 
-#initialize the bot
+# Initialize the bot
 intents = discord.Intents.all()
 intents.message_content = True #NOQA
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-#load the commands
+# Load the commands
 ping.ping(bot)
 warn.warn(bot)
 mod_event.mod_event(bot)
 
-#run the bot
-bot.run(TOKEN)
+# Error handling
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        pass
+    elif isinstance(error, commands.MissingPermissions):
+        pass
+        
+
+# Run the bot
+try:
+    bot.run(TOKEN)
+except Exception as e:
+    pass
